4a96d5ab27601569095ee1f832c474c6fb571342
Thomas Habets <thomas@habets.se>
2024-08-28 22:14:24 +0100

    Add cargo publish script

---
+++ extra/publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+if [[ -n "$(git status --porcelain)" ]]; then
+        echo "Local repo not clean"
+        git status
+        exit 1
+fi
+PATCH="$(sed -rn '/^version/{s/.*= *"[0-9]+[.][0-9]+[.]([0-9]+)"/\1/;p}' Cargo.toml)"
+PATCH=$(( PATCH + 1 ))
+sed -ri 's/^(version = "[0-9]+[.][0-9])[.].*/\1'.$PATCH'"/' Cargo.toml
+VER="$(sed -rn '/^version/{s/.*= *"([0-9]+[.][0-9]+[.][0-9]+)"/\1/;p}' Cargo.toml)"
+git add Cargo.toml
+git commit -a -m"Bump version to ${VER}"
+exec cargo publish
