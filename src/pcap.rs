b12b3d16758309f8a1a7d4da8db42517998c8e3f
Thomas Habets <thomas@habets.se>
2024-09-14 00:01:42 +0100

    pcap: Add a bunch of comments

--- src/pcap.rs
+++ src/pcap.rs
@@ -1,36 +1,21 @@
-/*
- *
- * man pcap-savefile
- *
- * For now use LINKTYPE_AX25, or number 3, with just AX.25.
- *
- * In the future maybe use LINKTYPE_AX25_KISS, 202.
- *
- * global header.
- *
-     typedef struct pcap_hdr_s {
-            guint32 magic_number;   /* magic number */
-            guint16 version_major;  /* major version number */
-            guint16 version_minor;  /* minor version number */
-            gint32  thiszone;       /* GMT to local correction */
-            guint32 sigfigs;        /* accuracy of timestamps */
-            guint32 snaplen;        /* max length of captured packets, in octets */
-            guint32 network;        /* data link type */
-    } pcap_hdr_t;
-
- typedef struct pcaprec_hdr_s {
-            guint32 ts_sec;         /* timestamp seconds */
-            guint32 ts_usec;        /* timestamp microseconds */
-            guint32 incl_len;       /* number of octets of packet saved in file */
-            guint32 orig_len;       /* actual length of packet */
-    } pcaprec_hdr_t;
-*/
+//! pcap writer
+//!
+//! The pcap format is very simple, so no need for an external crate or linking
+//! to libpcap.
+//!
+//! This implementation writes little endian pcap files on all platforms.
+//!
+//! Useful resources:
+//! * https://wiki.wireshark.org/Development/LibpcapFileFormat
+//! * https://www.ietf.org/archive/id/draft-gharris-opsawg-pcap-01.html
+//! * https://www.tcpdump.org/linktypes.html
 
 use std::io::BufWriter;
 use std::io::Write;
 
 use anyhow::Result;
 
+// Little endian magic.
 const MAGIC: [u8; 4] = [0xd4, 0xc3, 0xb2, 0xa1];
 const VERSION_MAJOR: u16 = 2;
 const VERSION_MINOR: u16 = 4;
@@ -51,10 +36,16 @@ fn write_u32(mut w: impl std::io::Write, v: u32) -> Result<()> {
     Ok(())
 }
 
+/// PcapWriter writes AX.25 pcap files.
+///
+/// It writes them buffered, for efficiency, so a crash could lose the last
+/// packets.
 pub struct PcapWriter {
     f: BufWriter<std::fs::File>,
 }
+
 impl PcapWriter {
+    /// Create a new pcap file. Fails if the file already exists.
     pub fn create(filename: std::path::PathBuf) -> Result<Self> {
         let mut f = BufWriter::new(
             std::fs::File::options()
@@ -66,13 +57,37 @@ impl PcapWriter {
         f.write_all(&MAGIC)?;
         write_u16(&mut f, VERSION_MAJOR)?;
         write_u16(&mut f, VERSION_MINOR)?;
-        write_u32(&mut f, 0)?; // GMT offset. TODO: actually i32.
-        write_u32(&mut f, 0)?; // figs?
-        write_u32(&mut f, 1500)?; // TODO: snaplen
+
+        // GMT offset. In theory this is i32 (not u32) GMT offset of all other
+        // timestamps in the file. In practice, who put anything but UTC
+        // timestamps in files?
+        //
+        // Actually, here's an idea: Buy another 32 bits of epoch offset, by
+        // using this field. "My time zone is offset from UTC by 68 years?
+        //
+        // Not sure what read tooling would support that though. My testing
+        // shows that wireshark seems to assume the closest wrap to today,
+        // because adding 20 years from today (2024) does show timestamps as
+        // being in 2044 in tshark -V.
+        write_u32(&mut f, 0)?;
+
+        // Time source accuracy. All tools set zero.
+        write_u32(&mut f, 0)?;
+
+        // Snaplen. This implementation simply captures the whole packets. And
+        // because it's AX.25 packets are more like 200 bytes.
+        write_u32(&mut f, 65536)?;
+
+        // Another option is LINKTYPE_AX25_KISS. But currently we have nothing
+        // interesting to populate there, so just using LINKTYPE_AX25 for now.
         write_u32(&mut f, LINKTYPE_AX25)?;
         Ok(Self { f })
     }
 
+    /// Write a blob as a new packet entry.
+    ///
+    /// If this write fails, no further writes can be made, as the added record
+    /// is now only partially added.
     pub fn write(&mut self, packet: &[u8]) -> Result<()> {
         let len = packet.len() as u32;
         let now = std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH)?;
@@ -85,3 +100,5 @@ impl PcapWriter {
         Ok(())
     }
 }
+/* vim: textwidth=80
+ */
